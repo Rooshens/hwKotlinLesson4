3870999dcf0acccec97f79239b87d0288f9a8968
aaaRooshen <aaa.rooshen@gmail.com>
2021-07-14 23:33:05 +0300

    1

---
+++ app/src/main/java/qr/a/rooshen/asiancuisine/MainActivity.kt
@@ -0,0 +1,17 @@
+package qr.a.rooshen.asiancuisine
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ScrollView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
